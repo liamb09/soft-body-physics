cbcc783a3cab3bd02bd7b24d3475f72994d44072
cod3r12 <85259706+cod3r12@users.noreply.github.com>
2025-06-30 11:49:46 -0500

    Linked shapes

--- include/SoftBodyPhysics2D.hpp
+++ include/SoftBodyPhysics2D.hpp
@@ -9,6 +9,7 @@
 class SoftBodyPhysics2D {
 
     std::vector<Shape> shapes;
+    std::vector<Spring> springs;
     float GRAVITY;
     float ELASTICITY; // 1 = perfectly elastic
 
@@ -24,10 +25,14 @@ public:
     void handleCollisions();
     void render (SDL_Renderer* &renderer);
 
-    void addShape (Shape &shape);
+    void addShape (Shape shape);
     void addShape (std::vector<Point> points, const bool &fixed = false);
+    void addShape (std::vector<Point>* points, const bool &fixed = false);
 
     // adds a rectangle with its top left corner at (x,y)
     void addRect (const float &x, const float &y, const float &width, const float &height, const float &m = 5, const bool &fixed = false);
+
+    void addLinkedShape (std::vector<std::vector<Point>> pointSets, std::vector<std::vector<std::pair<int, int>>> links, const std::vector<bool> &fixed);
+    void addLink (int iShape1, int iPoint1, int iShape2, int iPoint2); // indexes for shape and point to link to, iShape1 and iShape2 can be negative to go to last item (-1=last item, -2=second-to-last item)
 
 };
--- src/SoftBodyPhysics2D.cpp
+++ src/SoftBodyPhysics2D.cpp
@@ -15,6 +15,9 @@ void SoftBodyPhysics2D::update (const float &dt) {
     for (Shape &shape : shapes) {
         shape.update(dt, 0, 0, GRAVITY);
     }
+    for (Spring &spring : springs) {
+        spring.update(dt);
+    }
     handleCollisions();
 }
 void SoftBodyPhysics2D::handleCollisions() {
@@ -31,13 +34,21 @@ void SoftBodyPhysics2D::render (SDL_Renderer* &renderer) {
     for (Shape &shape : shapes) {
         shape.render(renderer);
     }
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    for (Spring &spring : springs) {
+        // std::cout << "rendering...\n";
+        spring.render(renderer);
+    }
 }
 
-void SoftBodyPhysics2D::addShape (Shape &shape) {
-    shapes.push_back(shape);
+void SoftBodyPhysics2D::addShape (Shape shape) {
+    shapes.push_back(std::move(shape));
 }
 void SoftBodyPhysics2D::addShape (std::vector<Point> points, const bool &fixed) {
     shapes.emplace_back(Shape(std::move(points), fixed));
+}
+void SoftBodyPhysics2D::addShape (std::vector<Point>* points, const bool &fixed) {
+    shapes.emplace_back(Shape(points, fixed));
 }
 
 void SoftBodyPhysics2D::addRect (const float &x, const float &y, const float &width, const float &height, const float &m, const bool &fixed) {
@@ -47,4 +58,22 @@ void SoftBodyPhysics2D::addRect (const float &x, const float &y, const float &wi
         Point(x+width, y+height, m),
         Point(x, y+height, m),
     }, fixed);
+}
+
+void SoftBodyPhysics2D::addLinkedShape (std::vector<std::vector<Point>> pointSets, std::vector<std::vector<std::pair<int, int>>> links, const std::vector<bool> &fixed) {
+    for (int i = 0; i < pointSets.size(); i++) {
+        addShape(pointSets[i], fixed[i]);
+    }
+    for (const std::vector<std::pair<int, int>> &link : links) {
+        addLink(
+            -(pointSets.size() - link[0].first), link[0].second,
+            -(pointSets.size() - link[1].first), link[1].second
+        );
+    }
+}
+void SoftBodyPhysics2D::addLink (int iShape1, int iPoint1, int iShape2, int iPoint2) {
+    // account for negative shape indices
+    iShape1 = (iShape1 + shapes.size())%shapes.size();
+    iShape2 = (iShape2 + shapes.size())%shapes.size();
+    springs.emplace_back(&(shapes[iShape1].getPoint(iPoint1)), &(shapes[iShape2].getPoint(iPoint2)), 750, 30);
 }
